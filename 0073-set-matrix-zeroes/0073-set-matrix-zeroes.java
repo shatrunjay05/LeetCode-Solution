3c5047a2412648410ed17f1d1c1297c176939830
Shatrunjay Kumar <100301792+Kumar230@users.noreply.github.com>
2023-07-18 01:04:16 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (31.05%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,42 +1,27 @@
-class Pair
-{
-    int i,j;
-    Pair(int i, int j)
-    {
-        this.i = i;
-        this.j = j;
-    }
-}
 class Solution {
-    public void setZeroes(int[][] matrix) 
-    {
-       Queue<Pair> p = new LinkedList<>();
-       int n = matrix.length;
-       int m = matrix[0].length;
-       for(int i=0; i<n; i++)
-       {
-           for(int j=0; j<m; j++)
-           {
-               if(matrix[i][j]==0)
-               {
-                   p.add(new Pair(i,j));
-               }
-           }
-       } 
-       while(!p.isEmpty())
-       {
-           Pair a = p.poll();
-           int i1 = a.i;
-           int j1 = a.j;
-           for(int k=0;k<m;k++)
-           {
-               matrix[i1][k]=0;
-           }
-           for(int k=0;k<n;k++)
-           {
-               matrix[k][j1]=0;
-           }
-       }
-
+    public int[][] setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int row[] = new int[n];
+        int col[] = new int[m];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
     }
 }
